5e8ea51b0e24ee68b7ebe08be1f7cf2f54f07c45
robertjinchun <robertjinchun@outlook.com>
2019-06-04 17:07:50 -0400

    Everyone was working on the axios call and promise and catch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,64 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component () {
-  render(){
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoading: true,
+      firstLetterResult:[]
+    }
+    console.log('Hello1');
+
+  }
+  axiosCall = function () {
+    let temporaryList = [];
+    let variable = 's';
+    axios({
+      //The API has no other way of sending exact data. I need to call the whole database.
+      url: `http://api.datamuse.com/sug?max=1000&s=${variable}`,
+      method: 'GET',
+      data: {
+        max: 1000,
+        // s: ""
+        // s: "userInput"
+      }
+    }).then((response) => {
+        temporaryList = response;
+        console.log('Then');
+        console.log(response);
+        this.setState({
+          firstLetterResult: temporaryList,
+        })
+      })
+      .catch(function (error) {
+        console.log('CrazyTown');
+      })
+      // Setting up parameters for if the promise is fulfilled or not
+          
+          
+          // isLoading: fa
+        // slotApp.koreanToronto = resultOfKoreanToro
+  }
+  // AJAX call for Mexican Restaurants in Mississauga
+
+
+  componentDidMount() {
+    console.log('Hello3');
+    this.axiosCall()
+
+  }
+
+  render() {
+    console.log('Hello2');
     return (
-      <div className="App">
-        <h1>Autocomplete</h1>
+
+      <div className='App'>
+        
       </div>
     );
   }
- 
 }
 
 export default App;
