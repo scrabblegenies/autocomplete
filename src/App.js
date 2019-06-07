d796d71faec7e028788c33315e27950d44d15b1c
Lobst3rG3off <erip.pasei@gmail.com>
2019-06-07 01:38:14 -0400

    It's a working clock!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import UserInput from './UserInput.js';
 // import UserOutput from './UserOutput.js'
+import Clock from './Clock.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuestionCircle} from '@fortawesome/free-solid-svg-icons'
 import { faWindowMinimize } from '@fortawesome/free-solid-svg-icons'
@@ -101,6 +102,7 @@ class App extends Component {
         <footer>
           <img src="assets/windows.png" alt="retro windows logo"/>
           <p class="start">Start</p> 
+           <Clock />
         </footer>
       </div>
     );
---
+++ src/Clock.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class Clock extends Component {
+     state = {
+         time: new Date()
+     };
+
+     componentDidMount() {
+         this.timerID = setInterval(() => this.tick(), 1000);
+     }
+
+     componentWillUnmount() {
+         clearInterval(this.timerID);
+     }
+
+     tick() {
+         this.setState({
+             time: new Date()
+         });
+     }
+
+  render() {
+    return (
+      <div>
+         <h2>{this.state.time.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+
+export default Clock;
