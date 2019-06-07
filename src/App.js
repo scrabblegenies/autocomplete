c4b24d93fa979bb4ecd1a3a4b4998ded3d2a3674
rebrobinson <rebrobinson>
2019-06-07 11:57:08 -0400

    fixing css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import axios from 'axios';
 import './App.css';
 import UserInput from './UserInput.js';
 import UserOutput from './UserOutput.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+import { faWindowMinimize } from '@fortawesome/free-solid-svg-icons'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
+const questionMarkIcon = <FontAwesomeIcon aria-hidden="true" icon={faQuestionCircle} />
+const minimizeIcon = <FontAwesomeIcon aria-hidden="true" icon={faWindowMinimize} />
+const closeWindowIcon = <FontAwesomeIcon aria-hidden="true" icon={faWindowClose} />
 
 class App extends Component {
 
@@ -70,15 +78,26 @@ componentDidMount() {
 render() {
   return (
     <div className='App'>
-      <div className = "userInput">
-        <UserInput
-          onChange={this.handleTextChange}
-          data={this.state.data} />
+      <div className="container">
+        <div className="wrapper">
+          <div className="tabBar">
+            <div className="iconContainer">
+              <p class="icon">{questionMarkIcon}</p>
+              <p class="icon">{minimizeIcon}</p>
+              <p class="icon">{closeWindowIcon}</p>
+            </div>
+          </div>
+          <div className="searchEngine">
+            <h1>Autocomplete</h1>
+            <UserInput onChange={this.handleTextChange} data={this.state.data} />
+            <UserOutput wholeWordResult={this.state.wholeWordResult} />
+          </div>
+        </div>
       </div>
-      <div className="UserOutput">
-        <UserOutput
-           wholeWordResult={this.state.wholeWordResult}/> 
-      </div>
+        <footer>
+          <img src="assets/windows.png" alt="retro windows logo" />
+          <p class="start">Start</p>
+        </footer>
       
     </div>
   );
@@ -86,3 +105,19 @@ render() {
 }
 
 export default App;
+
+
+
+
+{/* <div className='App'>
+  <div className="userInput">
+    <UserInput
+      onChange={this.handleTextChange}
+      data={this.state.data} />
+  </div>
+  <div className="UserOutput">
+    <UserOutput
+      wholeWordResult={this.state.wholeWordResult} />
+  </div>
+
+</div> */}
