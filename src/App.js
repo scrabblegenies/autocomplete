db0acd52d1db2f5b0cb4ca4d921fded293d49992
Lobst3rG3off <erip.pasei@gmail.com>
2019-06-09 16:20:10 -0400

    added favicon and fixed footer height

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import UserOutput from './UserOutput.js'
 
 
 
+
 const questionMarkIcon = <FontAwesomeIcon aria-hidden="true" icon={faQuestionCircle} />
 const minimizeIcon = <FontAwesomeIcon aria-hidden="true" icon={faWindowMinimize} />
 const closeWindowIcon = <FontAwesomeIcon aria-hidden="true" icon={faWindowClose} />
