dcd0c6645ae598f3e39cf80333e3705ffbb14acf
Lobst3rG3off <erip.pasei@gmail.com>
2019-06-10 15:27:20 -0400

    Updates

---
+++ precache-manifest.d680226cca9cf466de3d47cd502659bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1035ed1371d919925ca34add6db70962",
+    "url": "/scrabbleGenie/index.html"
+  },
+  {
+    "revision": "dc09d5a5941914562edf",
+    "url": "/scrabbleGenie/static/css/main.37e9c16a.chunk.css"
+  },
+  {
+    "revision": "575bd49d65381ff19b1f",
+    "url": "/scrabbleGenie/static/js/2.169bfb91.chunk.js"
+  },
+  {
+    "revision": "dc09d5a5941914562edf",
+    "url": "/scrabbleGenie/static/js/main.388de686.chunk.js"
+  },
+  {
+    "revision": "63ee0e015706e28c8a35",
+    "url": "/scrabbleGenie/static/js/runtime~main.eb5d7ccd.js"
+  },
+  {
+    "revision": "23d5e9bf7e2ea306403c5589d5858b8f",
+    "url": "/scrabbleGenie/static/media/windows.23d5e9bf.png"
+  }
+]);
