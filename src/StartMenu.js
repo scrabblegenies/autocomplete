bc54c4c920bea4681a5b088d72d2f1913572577f
rebrobinson <rebrobinson>
2019-06-10 14:16:13 -0400

    twitter links

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -14,11 +14,11 @@ class StartMenu extends Component {
                 className={visibility}>
                 <div className="sideBanner"></div>
                 <ul>
-                    <li>Robert Chun</li>
-                    <li>Geoff Grossman</li>
-                    <li>Laura Chuang</li>
-                    <li>Becky Robinson</li>
-                    <li className="datamuse">Made using the Datamuse API.</li>
+                    <li><a href="https://twitter.com/RobertJinChun">Robert Chun</a></li>
+                    <li><a href="https://twitter.com/Lobst3rDev">Geoff Grossman</a></li>
+                    <li><a href="https://twitter.com/LC_butWithCode">Laura Chuang</a></li>
+                    <li><a href="https://twitter.com/beckyiscoding">Becky Robinson</a></li>
+                    <li className="datamuse"><a href="https://www.datamuse.com/api/">Made using the Datamuse API.</a></li>
                 </ul>
             </div>
         )
