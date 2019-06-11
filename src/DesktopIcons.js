5a4ef7c7a01ad9338c17ba2e7c3f4ca687735559
snarkshow <laura.hw.chuang@gmail.com>
2019-06-11 15:09:56 -0400

    responsive instructions

--- src/DesktopIcons.js
+++ src/DesktopIcons.js
@@ -15,19 +15,19 @@ class DesktopIcons extends Component {
                     <button aria-label="about this app" className="instructions" onClick={handleInstructionsClick}><img src={icon1} aria-hidden="true"/></button>
                     <p className="iconText">Autocomplete Instructions</p>
                 </div>
-                <div className="iconContainer2">
+                <div className="iconContainer2 blankIcon">
                     <img src={icon2} className="icon2" aria-hidden="true" />
                     <p className="iconText">Don't click me</p>
                 </div>
-                <div className="iconContainer3">
+                <div className="iconContainer3 blankIcon">
                     <img src={icon3} className="icon3" aria-hidden="true" />
                     <p className="iconText">I do nothing</p>
                 </div>
-                <div className="iconContainer4">
+                <div className="iconContainer4 blankIcon">
                     <img src={icon4} className="icon4" aria-hidden="true" />
                     <p className="iconText">Ignore me</p>
                 </div>
-                <div className="iconContainer6">
+                <div className="iconContainer6 blankIcon">
                     <img src={icon6} className="icon6" aria-hidden="true" />
                     <p className="iconText">Just an icon</p>
                 </div>
