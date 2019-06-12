7583c3a9ec845158dc87acb82ea1274354631770
snarkshow <laura.hw.chuang@gmail.com>
2019-06-12 08:50:47 -0400

    Updates

---
+++ precache-manifest.e8ee5ed2812c98bac7f7e34401c8a3bd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8b0f46e9fc947f84e2ceebe763d288",
+    "url": "/autocomplete/index.html"
+  },
+  {
+    "revision": "f5b78e733e8d27de8bd6",
+    "url": "/autocomplete/static/css/main.611b8f24.chunk.css"
+  },
+  {
+    "revision": "0a939ebe501dbc595cf3",
+    "url": "/autocomplete/static/js/2.52c03034.chunk.js"
+  },
+  {
+    "revision": "f5b78e733e8d27de8bd6",
+    "url": "/autocomplete/static/js/main.bd07da83.chunk.js"
+  },
+  {
+    "revision": "fd43afae2742744004aa",
+    "url": "/autocomplete/static/js/runtime~main.921317ba.js"
+  },
+  {
+    "revision": "bac2d56118c329608d338c00a3dd95d5",
+    "url": "/autocomplete/static/media/icon1.bac2d561.png"
+  },
+  {
+    "revision": "14134a94595f6c52fc0921db367655d2",
+    "url": "/autocomplete/static/media/icon2.14134a94.png"
+  },
+  {
+    "revision": "dffa7899699468631ecdc1f3e900658d",
+    "url": "/autocomplete/static/media/icon3.dffa7899.png"
+  },
+  {
+    "revision": "7fbdffbe1d3e025db08772084a8e1817",
+    "url": "/autocomplete/static/media/icon4.7fbdffbe.png"
+  },
+  {
+    "revision": "53c95952cf171a476dbb1b60ef030dbd",
+    "url": "/autocomplete/static/media/icon5.53c95952.png"
+  },
+  {
+    "revision": "d2919305e5381772019f7d553d67c0fb",
+    "url": "/autocomplete/static/media/icon6.d2919305.png"
+  },
+  {
+    "revision": "23d5e9bf7e2ea306403c5589d5858b8f",
+    "url": "/autocomplete/static/media/windows.23d5e9bf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scrabbleGenie/precache-manifest.0bfa922b814c29c477cfed39a2e8ede5.js"
+  "/autocomplete/precache-manifest.e8ee5ed2812c98bac7f7e34401c8a3bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scrabbleGenie/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/autocomplete/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
